6d7e9d6ce3d5fe00a424bacfc47fce801ac56cec
Mason Daugherty <mdaugher@bowdoin.edu>
2025-04-08 01:11:41 -0400

    Update API documentation routes in Nginx configuration: add Swagger UI and OpenAPI JSON endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,8 @@
 """
-The backend is connected to the archive file system (as read-only) and
-provides an API to list and download recordings. As needed (determined
-by the request), concatenation of multiple files into a single download
-is supported for gapless playback across a given time range.
-
-Processing includes:
-- Computing the SHA-256 hash of the file.
-- Storing the metadata in the database.
+The backend is connected to the archive file system (read-only) and
+provides an endpoints to list and download recordings. For downloads
+containing multiple segments, we concatenate them together into a single
+gapless recording.
 
 Absolute paths should not be used. Instead, use `ARCHIVE_BASE` to refer
 to the archive root directory.
@@ -16,22 +12,15 @@ archive. Administrators have full access to the archive and can perform
 administrative tasks such as hiding recordings.
 
 Admin users are identified by a secret token that is passed in the
-`X-Admin-Token` header. An admin user table is stored in the database,
-which maps tokens to user IDs. An endpoint is provided to add new admin
-users, provided a super-admin token is passed in the
-`X-Super-Admin-Token` header (which is hardcoded in the service).
+`X-Admin-Token` header. This is defined in the `.env` file. The token is
+used to authenticate admin users and is required for all admin routes.
+
+Example usage:
+```bash
+curl -i -H "X-Admin-Token: changeme" http://localhost/api/admin/
+```
 
 NOTE: All routes are prefixed with `/api`.
-
-TODO:
-- Record fields from file (using ffprobe):
-    - bit_rate
-    - sample_rate
-    - icy-br
-    - icy-genre
-    - icy-name
-    - icy-url
-    - encoder
 """
 
 from app.api.routes.admin import router as admin_router
@@ -48,8 +37,7 @@ api_router.include_router(admin_router, prefix="/admin")
 
 def lifespan(_app: FastAPI):
     """
-    Start/stop the RabbitMQ consumer thread when the FastAPI app
-    starts/stops.
+    Start/stop the RabbitMQ consumer thread when the app starts/stops.
     """
     stop_event, consumer_thread = start_consumer_thread()
     yield
@@ -57,5 +45,5 @@ def lifespan(_app: FastAPI):
     consumer_thread.join()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, docs_url="/docs/", openapi_url="/openapi.json")
 app.include_router(api_router)
