e2d4c4cabe313a647d86f780b38b9b97e1d2729a
Mason Daugherty <mdaugher@bowdoin.edu>
2025-04-08 01:12:52 -0400

    Add backend application password to environment variables and improve admin route verification

--- backend/app/api/routes/admin.py
+++ backend/app/api/routes/admin.py
@@ -1,13 +1,27 @@
 """
 Admin protected routes.
+
+TODO: endpoints for:
+- block/unblock recordings
 """
 
-from fastapi import APIRouter
+from app.config import settings
+from fastapi import APIRouter, Depends, Header, HTTPException
 
 router = APIRouter()
 
 
-@router.get("/")
+def verify_admin(x_admin_token: str = Header(...)):
+    """
+    Naive admin token verification.
+    """
+    valid_admin_token = settings.BACKEND_APP_PASS
+    if x_admin_token != valid_admin_token:
+        raise HTTPException(status_code=401, detail="Invalid admin token")
+    return True
+
+
+@router.get("/", dependencies=[Depends(verify_admin)])
 def home():
     """
     Admin status check endpoint.
